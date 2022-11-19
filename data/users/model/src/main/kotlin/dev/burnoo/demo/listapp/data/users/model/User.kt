1baffdc5fd6aaf34ff4f4101dbd2b96066817a30
Bruno Wieczorek <bruno.wieczorek@gmail.com>
2022-11-19 13:45:10 +0100

    Add User models to :data:users:model module

---
+++ data/users/model/src/main/kotlin/dev/burnoo/demo/listapp/data/users/model/User.kt
@@ -0,0 +1,11 @@
+package dev.burnoo.demo.listapp.data.users.model
+
+data class User(
+    val title: String,
+    val firstName: String,
+    val lastName: String,
+    val photoUrl: String,
+    val gender: String,
+    val email: String,
+    val phone: String,
+)
