60c9713a5d9fd039b2cb87e6a7f6518cdb9704bd
Bruno Wieczorek <bruno.wieczorek@gmail.com>
2022-11-19 13:58:25 +0100

    Add UsersRepository and NetworkUsersRepository
    UsersRepository is implemented by NetworkUsersRepository, which is using only one
    DataSource - UsersNetworkDataSource. Test were added in NetworkUsersRepositoryTest.

---
+++ data/users/repository/src/main/kotlin/dev/burnoo/demo/listapp/data/users/repository/mappers/UserItem.kt
@@ -0,0 +1,13 @@
+package dev.burnoo.demo.listapp.data.users.repository.mappers
+
+import dev.burnoo.demo.listapp.data.users.model.UserId
+import dev.burnoo.demo.listapp.data.users.model.UserItem
+import dev.burnoo.demo.listapp.data.users.network.model.NetworkUserItem
+
+fun NetworkUserItem.asExternalModel() = UserItem(
+    id = UserId(id),
+    title = title,
+    firstName = firstName,
+    lastName = lastName,
+    photoUrl = picture,
+)
---
+++ data/users/repository/src/test/kotlin/dev/burnoo/demo/listapp/data/users/repository/NetworkUsersRepositoryTest.kt
@@ -0,0 +1,46 @@
+package dev.burnoo.demo.listapp.data.users.repository
+
+import dev.burnoo.demo.listapp.data.users.model.UserId
+import dev.burnoo.demo.listapp.data.users.repository.utils.FakeUsersNetworkDataSource
+import dev.burnoo.demo.listapp.data.users.repository.utils.testNetworkUser
+import dev.burnoo.demo.listapp.data.users.repository.utils.testNetworkUsers
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class NetworkUsersRepositoryTest {
+
+    private val fakeDataSource = FakeUsersNetworkDataSource()
+    private val repository = NetworkUsersRepository(
+        dataSource = fakeDataSource,
+        coroutineDispatcher = Dispatchers.Unconfined,
+    )
+
+    @Test
+    fun `should get users successfully`() = runBlocking {
+        val users = repository.getUsers()
+
+        users.forEachIndexed { index, userItem ->
+            userItem.id.value shouldBe testNetworkUsers[index].id
+            userItem.firstName shouldBe testNetworkUsers[index].firstName
+            userItem.lastName shouldBe testNetworkUsers[index].lastName
+            userItem.title shouldBe testNetworkUsers[index].title
+            userItem.photoUrl shouldBe testNetworkUsers[index].picture
+        }
+    }
+
+    @Test
+    fun `should get user successfully`() = runBlocking {
+        val userId = UserId(value = "TEST_USER_ID")
+        val user = repository.getUser(userId)
+
+        user.title shouldBe testNetworkUser.title
+        user.firstName shouldBe testNetworkUser.firstName
+        user.lastName shouldBe testNetworkUser.lastName
+        user.photoUrl shouldBe testNetworkUser.picture
+        user.gender shouldBe testNetworkUser.gender
+        user.email shouldBe testNetworkUser.email
+        user.phone shouldBe testNetworkUser.phone
+    }
+}
