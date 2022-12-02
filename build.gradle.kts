bd2f3cbd6de5816834a192f71fc516bd2695628a
Bruno Wieczorek <bruno.wieczorek@gmail.com>
2022-12-02 23:49:11 +0100

    Remove useless sourceSets from KMP modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
@@ -13,6 +14,23 @@ buildscript {
     dependencies {
         classpath(libs.gradle.android)
         classpath(libs.gradle.kotlin)
+        classpath(libs.gradle.composeMultiplatform)
+    }
+}
+
+allprojects {
+    // https://discuss.kotlinlang.org/t/disabling-androidandroidtestrelease-source-set-in-gradle-kotlin-dsl-script/21448/5
+    afterEvaluate {
+        project.extensions.findByType<KotlinMultiplatformExtension>()?.let { kmpExtension ->
+            kmpExtension.sourceSets.removeAll {
+                setOf(
+                    "androidAndroidTestRelease",
+                    "androidTestFixtures",
+                    "androidTestFixturesDebug",
+                    "androidTestFixturesRelease",
+                ).contains(it.name)
+            }
+        }
     }
 }
 
