d9ce2bb43c28d07265293d4d68c43d9defe51656
Bruno Wieczorek <bruno.wieczorek@gmail.com>
2022-12-19 17:32:51 +0100

    Change PagerTest from jvm to common

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -10,7 +10,7 @@ kotlin.sourceSets {
         }
     }
 
-    val jvmTest by getting {
+    val commonTest by getting {
         dependencies {
             implementation(kotlin("test"))
             implementation(libs.kotest.assertions)
